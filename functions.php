d0d3f353585c28974168a1dd9f9bca7176eeddaa
[lara-app] <[imranmmcbd@gmail.com]>
2021-10-16 13:50:08 +0600

    o-u

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ add_theme_support('title-tag');
 
 function imran_css_js_file_calling(){
     // CSS File
-    wp_enqueue_style('imran_style', get_stylesheet_uri());
+    wp_enqueue_style('imran_style', get_stylesheet_uri() );
     wp_register_style('bootstarp', get_template_directory_uri().'/css/bootstrap.css', array(), '5.1.3', 'all');
     wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.1', 'all');
     wp_enqueue_style('bootstarp');
--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(  ); ?>
 </head>
-<body <?php body_class(); ?>
+<body <?php body_class(); ?>>
     
-
+<h1>Hi there</h1>
 
 <?php wp_footer(  ); ?>
 </body>
