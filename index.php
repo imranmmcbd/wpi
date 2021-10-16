0fc4584e21fcb120f33a3a8a5711d2108397f553
[lara-app] <[imranmmcbd@gmail.com]>
2021-10-16 11:23:31 +0600

    o_u

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
+<?php 
+/*
+Header Area
+*/
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(  )?>" class="no-js">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ) ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WPI-Theme</title>
