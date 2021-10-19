0aea3dd65e6ed43634a3ece1c48a6398e33f3dd0
[lara-app] <[imranmmcbd@gmail.com]>
2021-10-19 12:13:10 +0600

    o-u

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"</a>
+                <a href=""><img src="<?php echo get-get_theme_mod('imran_logo'); ?>"</a>
             </div>
             <div class="col-md-9">
                 
